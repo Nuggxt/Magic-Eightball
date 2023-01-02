e6932d5f95ea06eda0b13b73b28660478bdacd93
NuggxtDev <devnuggetcodes@gmail.com>
2023-01-02 00:00:55 -0600

    Got the disply div to output a number. Need it to output the text with answers instead.

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
-const userQuestion = '';
+let userQuestion = '';
 let eightBall = '';
-const randomNumber = '';
+let randomNumber = '';
 
-
+//eightBall= sets up the answers to be displayed on button click.
 if (randomNumber === 0) {
     eightBall = 'No shot LMAO';
 } else if (randomNumber === 1) {
-    eightBall = 'Sounds good to me!';
+    eightBall = 'Get Fucked...';
 } else if (randomNumber === 2) {
-    eightBall = 'Fucking A bud, Sounds fucking good to me!'
+    eightBall = 'Fucking A bud, Sounds fucking good to me!';
 } else if (randomNumber === 3) {
     eightBall = 'Not a fucking clue...';
 } else if (randomNumber === 4) {
-    eightBall = 'Just fucking try it already.......';
+    eightBall = 'Just fucking try it already...';
 }
 
-document.getElementById('btn').onclick = function () {
-    let randomNumber = Math.floor(Math.random() * 5);
-    document.getElementById('op').innerHTML = op;
-}
-
-// console.log(eightBall);
+document.getElementById('askButton').onclick = function() {
+    eightBall = Math.floor(Math.random() * 5);
+    document.getElementById('xLabel').innerHTML = eightBall;
+};
