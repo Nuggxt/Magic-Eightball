37349aa4c2d8a07c72fc9ffc49552161e699ff26
NuggxtDev <devnuggetcodes@gmail.com>
2023-01-08 01:39:19 -0600

    Fixed answers
    Got the results to display inside the div on the page. Removed the console logs on each result also. And did a minor update to the answer div to center the text.

--- index.js
+++ index.js
@@ -6,18 +6,14 @@ let askButton = function () { // Creates the function for the butoton click to a
     randomNumber = Math.floor(Math.random() * 5) // SEt the random number generation inside the randomNumber variable.
     if (randomNumber === 0) {
         eightBall = 'No fucking shot LMAO!';
-        console.log(eightBall)
     } else if (randomNumber === 1) {
         eightBall = 'Why even still try?';
-        console.log(eightBall)
     } else if (randomNumber === 2) {
         eightBall = 'Cant even fucking begin to tell you...';
-        console.log(eightBall)
-    } else if(randomNumber === 3) {
+    } else if (randomNumber === 3) {
         eightBall = 'You have no future';
-        console.log(eightBall)
-    } else if(randomNumber === 4) {
+    } else if (randomNumber === 4) {
         eightBall = 'Fuck off';
-        console.log(eightBall)
     }
-}
+    document.getElementById('answer').innerHTML = eightBall; // Displays the answer inside of the div for answers
+};
