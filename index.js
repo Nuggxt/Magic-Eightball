b87e03547e103510060b3d5e9282108e804eea83
NuggxtDev <devnuggetcodes@gmail.com>
2023-01-11 00:24:45 -0600

    Update to the javascript
    I added the option to add a name on page load so it displays the name with the response. I also removed the if...else and used a switch statement instead.

--- index.js
+++ index.js
@@ -1,28 +1,42 @@
-let eightBall = "";
 const userName = prompt("What is your name?"); // Asks for users name in a prompt. Will change later to where there is no prompt and user just types name on the page.
+eightBall = "";
 
 const askButton = function () {
-    randomNumber = Math.floor(Math.random() * 10) // Random number gen.
-    if (randomNumber === 0) {
-        eightBall = "No fucking shot";
-    } else if (randomNumber === 1) {
-        eightBall = "Ask again";
-    } else if (randomNumber === 2) {
-        eightBall = "Don\’t think, you might sprain your brain";
-    } else if (randomNumber === 3) {
-        eightBall = "You are useless";
-    } else if (randomNumber === 4) {
-        eightBall = "Your ideas never work";
-    } else if (randomNumber === 5) {
-        eightBall = "Your goals are unattainable";
-    } else if (randomNumber === 6) {
-        eightBall = "You irritate me";
-    } else if (randomNumber === 7) {
-        eightBall = "I don\'t care";
-    } else if (randomNumber === 8) {
-        eightBall = "You are too needy";
-    } else if (randomNumber === 9) {
-        eightBall = "Deal with it";
+    randomNumber = Math.floor(Math.random() * 10)
+    switch (randomNumber) {
+        case 0:
+            eightBall = "No Fucking Shot ";
+            break;
+        case 1:
+            eightBall = "Ask again later ";
+            break;
+        case 2:
+            eightBall = "You are worthless "
+            break;
+        case 3:
+            eightBall = "Your ideas will never work "
+            break;
+        case 4:
+            eightBall = "Your goals are unattainable "
+            break;
+        case 5:
+            eightBall = "Why do you keep asking questions? "
+            break;
+        case 6:
+            eightBall = "Very doubtful "
+            break;
+        case 7:
+            eightBall = "Outlook not so good "
+            break;
+        case 8:
+            eightBall = "My reply is no "
+            break;
+        case 9:
+            eightBall = "Don't count on it "
+            break;
+            case 10:
+                eightBall = "Cannot predict now "
+                break;
     }
     document.getElementById("answer").innerHTML = eightBall + userName;
 };
